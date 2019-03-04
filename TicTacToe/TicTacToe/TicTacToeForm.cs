6ab07a7b973082657674bf5691cd3e016985dafd
Admin <Admin@BS>
2019-03-04 16:35:59 +0300

    In progress AI Master with play O

--- TicTacToe/TicTacToe/TicTacToeForm.cs
+++ TicTacToe/TicTacToe/TicTacToeForm.cs
@@ -55,10 +55,12 @@ namespace TicTacToe
                     {
                         if (radioButtonAINovice.Checked)
                             randomButton(lastStep ? "O" : "X");
-                        else
+                        else if (radioButtonX.Checked)
                         {
                             chooseAI("X");
                         }
+                        else
+                            chooseAI("O");
                         if (lastStep)
                             lastStep = false;
                         else
@@ -204,14 +206,14 @@ namespace TicTacToe
 
         void chooseAI(string s)
         {
-            if (s=="X")
+            if (s == "X")
             {
                 Random r = new Random();
                 switch (countSteps)
                 {
-                    case 2:                        
+                    case 2:
                         if (button10.Text == "O")
-                        {                            
+                        {
                             if (r.Next(1, 3) == 1)
                                 button02.Text = "X";
                             else
@@ -238,10 +240,160 @@ namespace TicTacToe
                             else
                                 button02.Text = "X";
                         }
-
+                        else if (button00.Text == "O")
+                            button22.Text = "X";
+                        else if (button20.Text == "O")
+                            button02.Text = "X";
+                        else if (button02.Text == "O")
+                            button20.Text = "X";
+                        else if (button22.Text == "O")
+                            button00.Text = "X";
                         break;
-
+                    case 4:
+                        checkYourPosition("X", "X");
+                        if (countSteps == 5)
+                            countSteps--;
+                        else
+                        {
+                            checkYourPosition("O", "X");
+                        }
+                        if (countSteps == 5)
+                            countSteps--;
+                        else if (button00.Text == "O" && button12.Text == "O")
+                            button20.Text = "X";
+                        else if (button00.Text == "O" && button21.Text == "O")
+                            button02.Text = "X";
+                        else if (button02.Text == "O" && button21.Text == "O")
+                            button00.Text = "X";
+                        else if (button02.Text == "O" && button10.Text == "O")
+                            button22.Text = "X";
+                        else if (button20.Text == "O" && button01.Text == "O")
+                            button22.Text = "X";
+                        else if (button20.Text == "O" && button12.Text == "O")
+                            button00.Text = "X";
+                        else if (button22.Text == "O" && button10.Text == "O")
+                            button02.Text = "X";
+                        else if (button22.Text == "O" && button01.Text == "O")
+                            button20.Text = "X";
+                        break;
+                    case 6:
+                        checkYourPosition("X", "X");
+                        if (countSteps == 7)
+                            countSteps--;
+                        else
+                        {
+                            checkYourPosition("O", "X");
+                        }
+                        if (countSteps == 7)
+                            countSteps--;
+                        else if (button10.Text == "" && button12.Text == "")
+                        {
+                            if (r.Next(1, 3) == 1)
+                                button10.Text = "X";
+                            else
+                                button12.Text = "X";
+                        }
+                        else if (button01.Text == "" && button21.Text == "")
+                        {
+                            if (r.Next(1, 3) == 1)
+                                button01.Text = "X";
+                            else
+                                button21.Text = "X";
+                        }
+                        break;
+                    case 8:
+                        checkYourPosition("X", "X");
+                        if (countSteps == 9)
+                            countSteps--;
+                        else
+                        {
+                            checkYourPosition("O", "X");
+                        }
+                        if (countSteps == 9)
+                            countSteps--;
+                        else
+                            randomButton("X");
+                        break;
                 }
+            }
+            else
+            {
+                Random r = new Random();                
+                switch (countSteps)
+                {                    
+                    case 1:
+                        int a = r.Next(1, 5);
+                        if (button11.Text == "")
+                            button11.Text = "O";
+                        else if(a == 1)
+                                button00.Text = "O";
+                        else if (a == 2)
+                            button02.Text = "O";
+                        else if (a == 3)
+                            button20.Text = "O";
+                        else 
+                            button22.Text = "O";
+                        break;
+                    case 3:
+                        a = r.Next(1, 5);
+                        if (button00.Text == "" || button02.Text=="" || button20.Text=="" || button22.Text=="")
+                        {
+                            if (a == 1)
+                                button00.Text = "O";
+                            else if (a == 2)
+                                button02.Text = "O";
+                            else if (a == 3)
+                                button20.Text = "O";
+                            else
+                                button22.Text = "O";
+                        }
+                        break;
+                }
+            }
+        }
+
+        void checkYourPosition(string s, string t)
+        {
+            countSteps++;
+            if (button00.Text == s && button01.Text == s && button02.Text == "")
+                button02.Text = t;
+            else if (button00.Text == s && button02.Text == s && button01.Text == "")
+                button01.Text = t;
+            else if (button01.Text == s && button02.Text == s && button00.Text == "")
+                button00.Text = t;
+            else if (button00.Text == s && button10.Text == s && button20.Text == "")
+                button20.Text = t;
+            else if (button00.Text == s && button20.Text == s && button10.Text == "")
+                button10.Text = t;
+            else if (button10.Text == s && button20.Text == s && button00.Text == "")
+                button00.Text = t;
+            else if (button02.Text == s && button12.Text == s && button22.Text == "")
+                button22.Text = t;
+            else if (button02.Text == s && button22.Text == s && button12.Text == "")
+                button12.Text = t;
+            else if (button12.Text == s && button22.Text == s && button02.Text == "")
+                button02.Text = t;
+            else if (button20.Text == s && button21.Text == s && button22.Text == "")
+                button22.Text = t;
+            else if (button20.Text == s && button22.Text == s && button21.Text == "")
+                button21.Text = t;
+            else if (button22.Text == s && button21.Text == s && button20.Text == "")
+                button20.Text = t;
+            else if (button00.Text == s && button11.Text == s && button22.Text == "")
+                button22.Text = t;
+            else if (button00.Text == s && button22.Text == s && button11.Text == "")
+                button11.Text = t;
+            else if (button22.Text == s && button11.Text == s && button00.Text == "")
+                button00.Text = t;
+            else if (button20.Text == s && button11.Text == s && button02.Text == "")
+                button02.Text = t;
+            else if (button20.Text == s && button02.Text == s && button11.Text == "")
+                button11.Text = t;
+            else if (button02.Text == s && button11.Text == s && button20.Text == "")
+                button20.Text = t;
+            else
+            {
+                countSteps--;
             }
         }
     }
